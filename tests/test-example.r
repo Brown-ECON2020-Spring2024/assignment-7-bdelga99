4362e0e26237e78e9e8dc394053a9f9f2c6badec
bdelga99 <bdelga99@gmail.com>
2024-03-14 23:08:25 -0400

    Updated unit tests

--- tests/test-example.r
+++ tests/test-example.r
@@ -1,5 +1,12 @@
+library(testdat)
 test_that("This is an example test", {
   a <- 2
   b <- 10
   expect_equal(a * b, 20)
+  names(model$coefficients) <- NULL
+  expect_equal(model$coefficients[2], 0.00076, tolerance = 0.01)
+  expect_equal(nobs(model), 1704)
+  expect_unique(data = gapminder, c(country, year))
+  expect_range(data = gapminder, gdpPercap, 0, 1e6)
+  expect_values(data = gapminder, continent, c("Asia", "Europe", "Africa", "Americas", "Oceania"))
 })
